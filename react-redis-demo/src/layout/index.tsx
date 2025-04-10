ddcbb20ffae8a7fd3af642ff1aaea4a4a516e67e
liuhuan <1057016137@qq.com>
2025-04-10 20:22:33 +0800

    init:项目初始化

--- react-redis-demo/src/layout/index.tsx
+++ react-redis-demo/src/layout/index.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import './index.less';
 
 const { Header, Content, Footer, Sider } = Layout;
 
-const items1: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
-  key,
-  label: `nav ${key}`,
-}));
 
 const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
   (icon, index) => {
@@ -37,25 +34,16 @@ const LayoutBase: React.FC = () => {
   return (
     <Layout>
       <Header style={{ display: 'flex', alignItems: 'center' }}>
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['2']}
-          items={items1}
-          style={{ flex: 1, minWidth: 0 }}
-        />
+        <div className="logo-img" />
+        <div className='logo-title'>react-redis-test</div>
       </Header>
       <div style={{ margin: '0 auto', width: 'calc(100% - 32px)' }}>
         <Breadcrumb style={{ margin: '16px 0' }}>
-          {/* <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item> */}
         </Breadcrumb>
         <Layout
           style={{ padding: '24px 0', background: colorBgContainer, borderRadius: borderRadiusLG }}
         >
-          <Sider style={{ background: colorBgContainer, height: 'calc(100vh - 210px)', overflow: 'auto' }} width={200}>
+          <Sider style={{ background: colorBgContainer }} className='layout-sider'>
             <Menu
               mode="inline"
               defaultSelectedKeys={['1']}
@@ -64,11 +52,11 @@ const LayoutBase: React.FC = () => {
               items={items2}
             />
           </Sider>
-          <Content style={{ padding: '0 24px', height: 'calc(100vh - 210px)' }}>Content</Content>
+          <Content className='layout-content'>Content</Content>
         </Layout>
       </div>
-      <Footer style={{ textAlign: 'center' }}>
-        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+      <Footer className='layout-footer'>
+        react-redis-test ©{new Date().getFullYear()} Created by LIUHUAN
       </Footer>
     </Layout>
   );
